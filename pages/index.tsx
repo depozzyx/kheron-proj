72befba05efbfd7e85a33aa598f849c037462d08
depozzyx <mosentsowivan@gmail.com>
2021-09-23 22:33:12 +0300

    shit ver

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,23 @@
-import Layout from "@/components/layout/Layout";
+import Header from "@/components/header";
+
+import styles from "@/styles/Index.module.scss";
 import { useTexts } from "@/lib/hooks";
 import { getStaticPropsOnlyTexts } from "@/lib/texts_utils";
 
-export default function New() {
+export default function Index() {
     const { t } = useTexts();
 
     return (
-        <Layout t={t} title={t("index.title")} subtitle={t("index.subtitle")}>
-            <h1>Hi bruuuh</h1>
-            <p>nah</p>
-            <section>
-                <h1>bob</h1>
-                <p>sssss</p>
-            </section>
-        </Layout>
+        <>
+            <Header t={t}></Header>
+            <main className={styles.main}>
+                <div>hi bruuuh</div>
+                <div>hi bruuuh</div>
+                <div>hi bruuuh</div>
+                <div>hi bruuuh</div>
+                <div>hi bruuuh</div>
+            </main>
+        </>
     );
 }
 
